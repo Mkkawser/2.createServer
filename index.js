3fd0151349033cea73da662b6a32091c78b11e9a
Mkkawser <irumorgan420@gmail.com>
2023-12-13 09:13:37 +0600

    up

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http");
 const port = 3000;
 http
   .createServer((req, res) => {
-    res.write("hello");
+    res.write("hello world");
     console.log(`server connnected ${port}`);
     res.end();
   })
